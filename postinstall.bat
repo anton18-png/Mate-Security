99c7e2f3764b9c66189dd7ddf3efd6c409c7e0b2
Scidonchik <86909941+anton18-png@users.noreply.github.com>
2026-02-01 20:53:21 +0300

    🩹 Update postinstall

--- postinstall.bat
+++ postinstall.bat
@@ -6,5 +6,8 @@ curl -g -k -L -# -o "C:\Apps\Mate-Security\_internal\Utils\clamav.zip" "https://
 "C:\Apps\Mate-Security\_internal\Utils\7za.exe" x "C:\Apps\Mate-Security\_internal\Utils\clamav.zip" -o"C:\Apps\Mate-Security\_internal\Utils" -y
 robocopy "C:\Apps\Mate-Security\_internal\Utils\clamav-1.5.1.win.x64" "C:\Apps\Mate-Security\_internal\Utils\clamav" /E /MOVE /COPY:DAT /R:0 /W:0
 
+echo. > "C:\Apps\Mate-Security\_internal\Utils\clamav\clamd.log"
+echo. > "C:\Apps\Mate-Security\_internal\Utils\clamav\freshclam.log"
+
 rd /S /Q "C:\Apps\Mate-Security\_internal\Utils\clamav-1.5.1.win.x64"
 del /Q "C:\Apps\Mate-Security\_internal\Utils\clamav.zip"
